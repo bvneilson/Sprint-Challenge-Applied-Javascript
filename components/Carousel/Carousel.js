95d53613c1e2bbfae8340e24b544b57db8e2425e
Brennan Neilson <bvneilson@gmail.com>
2019-09-14 12:33:35 -0600

    Carousel stretch goal

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,48 @@
     <div class="right-button"> > </div>
   </div>
 */
+let images = ['./assets/carousel/mountains.jpeg', './assets/carousel/computer.jpeg', './assets/carousel/trees.jpeg', './assets/carousel/turntable.jpeg']
+function createCarousel (images) {
+  let current = 0;
+  const carousel = document.createElement('div');
+  const leftButton = document.createElement('div');
+  const img = document.createElement('img');
+  const rightButton = document.createElement('div');
+
+  carousel.classList.add('carousel');
+  leftButton.classList.add('left-button');
+  img.src = images[0];
+  rightButton.classList.add('right-button');
+
+  leftButton.textContent = ' < ';
+  rightButton.textContent = ' > ';
+
+  leftButton.addEventListener("click", function (event) {
+    if (current === 3) {
+      img.src = images[0];
+      current = 0;
+    } else {
+      img.src = images[current + 1];
+      current += 1;
+    }
+  })
+
+  rightButton.addEventListener("click", function (event) {
+    if (current === 0) {
+      img.src = images[3];
+      current = 3;
+    } else {
+      img.src = images[current - 1];
+      current -= 1;
+    }
+  })
+
+  carousel.appendChild(leftButton);
+  carousel.appendChild(img);
+  carousel.appendChild(rightButton);
+
+  return carousel;
+}
+
+const carouselContainer = document.querySelector('.carousel-container');
+carouselContainer.appendChild(createCarousel(images));
